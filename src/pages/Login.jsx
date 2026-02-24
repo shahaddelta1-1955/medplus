48388fabc215163f96a47cb35f9a6fcc81a46a94
shahaddelta2-2 <shahaddelta2@gmail.com>
2026-02-25 01:32:49 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -60,6 +60,9 @@ return (
 
     <div className="right-panel">
       <h2>Pharmacy Login</h2>
+     <div className="mobile-logo">
+  <img src={logo} alt="MedPlus Logo" />
+</div>
       <p className="subtitle">
         Access your MedPlus dashboard and manage your store.
       </p>
